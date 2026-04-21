1b372f4c8f578113ede13c19f2712dc3805ccef6
Brennan Stehling <277419+brennanMKE@users.noreply.github.com>
2026-04-20 21:48:38 -0700

    loops run sequence

--- scripts/run_sequence.sh
+++ scripts/run_sequence.sh
@@ -1,14 +1,16 @@
 #!/bin/zsh
 
-sleep 3
+while :; do
+sleep 1
 ./scripts/set_theme.sh all GREEN
-sleep 3
+sleep 1
 ./scripts/set_theme.sh all RAINBOW
-sleep 3
+sleep 1
 ./scripts/set_theme.sh all PINK_PONY
-sleep 3
-./scripts/set_theme.sh all GREEN
-sleep 3
-./scripts/set_theme.sh all RAINBOW
-sleep 3
-./scripts/set_theme.sh all PINK_PONY
+sleep 1
+./scripts/set_theme.sh all OCEAN_WAVES
+sleep 1
+./scripts/set_theme.sh all SUNSET
+sleep 1
+./scripts/set_theme.sh all FOREST
+done
